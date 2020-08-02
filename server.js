b6fe80c4246963a5470c6d9876f2adf6e016fc24
Abhishek Panchal <abhishekpanchal14@gmail.com>
2020-08-02 11:55:52 +0400

    Added db key

--- server.js
+++ server.js
@@ -18,9 +18,8 @@ app.use(expressLayouts)
 app.use(express.static('public'))
 
 const mongoose = require('mongoose')
-// mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true})
-mongoose.connect('mongodb+srv://mongodb:1234@abhishekmongodbcluster.dtpm0.azure.mongodb.net/mongodb?retryWrites=true&w=majority',
-{ useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true})
+
 const db = mongoose.connection
 db.on('error',error => console.error(error))
 db.once('open',() => console.log('Connected to Mongoose'))
