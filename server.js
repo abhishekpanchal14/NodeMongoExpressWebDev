22ba5bc7798444a2e683c4d60663f7add43418c4
Abhishek Panchal <abhishekpanchal14@gmail.com>
2020-08-02 17:02:07 +0400

    Initial Author Routes

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bodyParser = require('body-parser')
+
 var path = require ('path')
 
 app.set('view engine', 'ejs')
@@ -16,14 +19,18 @@ app.set('views', path.join(__dirname + '/views'))
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true})
+//mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true})
+mongoose.connect('mongodb+srv://mongodb:1234@abhishekmongodbcluster.dtpm0.azure.mongodb.net/mongodb?retryWrites=true&w=majority',
+{ useNewUrlParser: true})
 
 const db = mongoose.connection
 db.on('error',error => console.error(error))
 db.once('open',() => console.log('Connected to Mongoose'))
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
 
 app.listen(process.env.PORT || 3001)
