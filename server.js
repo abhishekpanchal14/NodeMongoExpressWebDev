189fc9b0b9610b0175445bf98322a10f1ee1eaa0
Abhishek Panchal <abhishekpanchal14@gmail.com>
2020-08-03 00:09:39 +0400

    Books Created Index/New

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ if(process.env.NODE_ENV !== 'production')
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+
 
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
-const bodyParser = require('body-parser')
+const bookRouter = require('./routes/books')
 
 var path = require ('path')
 
@@ -22,8 +24,9 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false }))
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true})
-
+//mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true})
+mongoose.connect('mongodb+srv://mongodb:1234@abhishekmongodbcluster.dtpm0.azure.mongodb.net/mongodb?retryWrites=true&w=majority',
+{ useNewUrlParser: true})
 
 const db = mongoose.connection
 db.on('error',error => console.error(error))
@@ -31,5 +34,6 @@ db.once('open',() => console.log('Connected to Mongoose'))
 
 app.use('/',indexRouter)
 app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
 
 app.listen(process.env.PORT || 3001)
